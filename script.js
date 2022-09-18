a71ea5ce2e43f72ad3736a61c4c7c3b593660ed2
wxyzlines <stoutshako42ref.zglj9@slmail.me>
2022-09-19 00:12:15 +0800

    added random color

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const pixelContainer = document.querySelector("#pixel-container")
-let colors = ['#ff0000', '#FF4433', '#FFA500', '#FFAE42', '#FFFF00', '#00FF00', '#32CD32', '#088F8F', '#0000ff', '#4B0082', '#8F00FF', '#fff', '#aaa', '#555', '#000']
+let colors = ['#ff0000', '#FF4433', '#FFA500', '#FFAE42', '#FFFF00', '#00FF00', '#32CD32', '#088F8F', '#0000ff', '#4B0082', '#8F00FF', '#aaa', '#555', '#000', '#fff']
 let currentColor = "black"
+let randomColorMode = false
 initializePixels(16,16)
 initializeColorPicker()
 
@@ -13,14 +14,20 @@ function initializePixels(sideCount) {
 
     const pixels = document.querySelectorAll('.pixel')
     pixels.forEach(pixel => {
-        pixel.addEventListener('mouseover', changePixel)
+        pixel.addEventListener('mouseenter', changePixel)
     })
 }
 
-
 function changePixel(e) {
-    if (e.buttons == 1)
-        this.style.backgroundColor = currentColor
+    if (e.buttons == 1) {
+        if (randomColorMode) {
+            this.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+        }
+        else {
+            this.style.backgroundColor = currentColor
+        }
+    }
+        
 }
 
 function changeDimension() {
@@ -50,9 +57,19 @@ function initializeColorPicker() {
         color.addEventListener('click', changeCurrentColor)
         colorContainer.appendChild(color)
     })
+
+    const randomColor = document.createElement("div")
+    randomColor.style.backgroundColor = `#124121`
+    randomColor.style.width = '32px'
+    randomColor.style.height = '32px'
+    randomColor.addEventListener('click', (e) => {
+        randomColorMode = true
+    })
+    colorContainer.appendChild(randomColor)
     
 }
 
 function changeCurrentColor(e) {
+    randomColorMode = false
     currentColor = this.style.backgroundColor
 }

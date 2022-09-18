7b7bc0f60e72ddca3e1edd3668fcd6b805937458
wxyzlines <stoutshako42ref.zglj9@slmail.me>
2022-09-18 23:55:33 +0800

    added color toolbox

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const pixelContainer = document.querySelector("#pixel-container")
+let colors = ['#ff0000', '#FF4433', '#FFA500', '#FFAE42', '#FFFF00', '#00FF00', '#32CD32', '#088F8F', '#0000ff', '#4B0082', '#8F00FF', '#fff', '#aaa', '#555', '#000']
+let currentColor = "black"
 initializePixels(16,16)
+initializeColorPicker()
+
 function initializePixels(sideCount) {
     for (let i = 0; i < sideCount*sideCount; i++) {
         const pixel = document.createElement("div")
@@ -9,20 +13,18 @@ function initializePixels(sideCount) {
 
     const pixels = document.querySelectorAll('.pixel')
     pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', changePixel)
-})
+        pixel.addEventListener('mouseover', changePixel)
+    })
 }
 
 
-
-
 function changePixel(e) {
-    this.classList.add("changed")
+    if (e.buttons == 1)
+        this.style.backgroundColor = currentColor
 }
 
 function changeDimension() {
     let sideCount = prompt("Enter pixel count per side.")
-
     while (true) {
         if (sideCount > 100 || sideCount < 16) {
             sideCount = prompt("Number must be between 16 and 100.\nEnter pixel count per side.")
@@ -35,4 +37,22 @@ function changeDimension() {
     initializePixels(sideCount)
     
 
+}
+
+function initializeColorPicker() {
+    const colorContainer = document.querySelector("#color-container")
+
+    colors.forEach(hex => {
+        const color = document.createElement("div")
+        color.style.backgroundColor = `${hex}`
+        color.style.width = '32px'
+        color.style.height = '32px'
+        color.addEventListener('click', changeCurrentColor)
+        colorContainer.appendChild(color)
+    })
+    
+}
+
+function changeCurrentColor(e) {
+    currentColor = this.style.backgroundColor
 }
